39240f36211fcfaba373b73d39d9b1b44af06c33
Harshit P G <harshitpg2004@gmail.com>
2024-06-02 12:29:07 +0530

    user login

--- cmd/service/user/routes.go
+++ cmd/service/user/routes.go
@@ -26,6 +26,26 @@ func (h *Handler) RegisterRoutes(router *mux.Router){
 }
 
 func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request){
+	var user types.LoginUserPayload
+	if err:= utils.ParseJSON(r, &user); err !=nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+	if err:= utils.Validate.Struct(user); err !=nil{
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload %v",errors))
+		return
+	}
+	u, err:= h.store.GetUserByEmail(user.Email)
+	if err != nil{
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("not found invalid email or password"))
+		return
+	}
+
+	if !auth.Comparepasswords(u.Password,[]byte(user.Password)){
+		utils.WriteError(w,http.StatusBadRequest,fmt.Errorf("not found invalid email or password"))
+	}
+
 
 }
 
